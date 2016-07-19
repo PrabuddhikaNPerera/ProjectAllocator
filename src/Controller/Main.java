7f8fd620050acced50cba3b38732447c115ecffa
Supun Praneeth <spmsupun@gmail.com>
2016-07-20 02:48:38 +0530

    Disappointment Calculate

--- src/Controller/Main.java
+++ src/Controller/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("../View/MainWindow.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("MainWindow.fxml"));
         primaryStage.setTitle("Project Allocation System");
         primaryStage.setScene(new Scene(root, 800, 500));
         primaryStage.show();
