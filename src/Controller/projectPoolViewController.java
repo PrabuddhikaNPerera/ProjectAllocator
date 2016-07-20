3014a13b43ac539213488d79df4d7bb3ddb2deac
Prabuddhika Niroshan <ppnperera@virtusa.com>
2016-07-20 19:23:24 +0530

    mod

--- src/Controller/projectPoolViewController.java
+++ src/Controller/projectPoolViewController.java
@@ -1,10 +1,13 @@
 package Controller;
 
+import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.ObservableList;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.stage.Stage;
 import javafx.stage.Window;
+import javafx.util.Callback;
 
 /**
  * Created by PPNPERERA on 7/16/2016.
@@ -16,7 +19,13 @@ public class projectPoolViewController {
 
     public void show(ObservableList<String> lefts){
         System.out.println(lefts);
-        //listView.setItems(lefts);
+        listView.setItems(lefts);
+        listView.setCellFactory(new Callback<ListView<String>, ListCell<String>>() {
+            @Override
+            public ListCell<String> call(ListView<String> param) {
+                return new ListCell<>();
+            }
+        });
     }
 
     public void close(){
